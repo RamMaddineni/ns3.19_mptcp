9e73cd0b0e0357e52d692f523159f6541db9a084
RamMaddineni <maddineniramkumar29@gmail.com>
2023-12-13 14:23:43 +0530

    t

---
+++ scratch/mptcpFullMesh.cc
@@ -0,0 +1,85 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Morteza Kheirkhah <m.kheirkhah@sussex.ac.uk>
+ */
+
+#include <string>
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/network-module.h"
+#include "ns3/packet-sink.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("MpTcpBulkSendExample");
+
+int main(int argc, char *argv[])
+{
+  LogComponentEnable("MpTcpSocketBase", LOG_INFO);
+
+  Config::SetDefault("ns3::Ipv4GlobalRouting::FlowEcmpRouting", BooleanValue(true));
+  Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(1400));
+  Config::SetDefault("ns3::TcpSocket::DelAckCount", UintegerValue(0));
+  Config::SetDefault("ns3::DropTailQueue::Mode", StringValue("QUEUE_MODE_PACKETS"));
+  Config::SetDefault("ns3::DropTailQueue::MaxPackets", UintegerValue(100));
+  Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue(MpTcpSocketBase::GetTypeId()));
+  Config::SetDefault("ns3::MpTcpSocketBase::MaxSubflows", UintegerValue(8)); // Sink
+  Config::SetDefault("ns3::MpTcpSocketBase::CongestionControl", StringValue("RTT_Compensator"));
+  Config::SetDefault("ns3::MpTcpSocketBase::PathManagement", StringValue("NdiffPorts"));
+
+  NodeContainer nodes;
+  nodes.Create(5); // Create 5 nodes
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+  pointToPoint.SetChannelAttribute("Delay", StringValue("1ms"));
+
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install(nodes);
+
+  InternetStackHelper internet;
+  internet.Install(nodes);
+
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer i = ipv4.Assign(devices);
+
+  uint16_t port = 9;
+
+  // Create a full mesh topology by connecting each node to every other node
+  for (uint32_t i = 0; i < nodes.GetN(); ++i)
+  {
+    for (uint32_t j = i + 1; j < nodes.GetN(); ++j)
+    {
+      PointToPointHelper meshLink;
+      meshLink.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+      meshLink.SetChannelAttribute("Delay", StringValue("1ms"));
+
+      NetDeviceContainer meshDevices;
+      meshDevices = meshLink.Install(nodes.Get(i), nodes.Get(j));
+    }
+  }
+
+  // Add applications as needed
+
+  NS_LOG_INFO("Run Simulation.");
+  Simulator::Stop(Seconds(20.0));
+  Simulator::Run();
+  Simulator::Destroy();
+  NS_LOG_INFO("Done.");
+}
